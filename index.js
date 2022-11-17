4e833a834768006effff4be1960cf26b67ce37d6
Nikhil Vyas <nikhil.vyas@taskcraft.in>
2022-11-17 18:44:40 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// var http = require('http');
+// var fs = require('fs');
+
+// http.createServer(function (req, res) {
+//     fs.readFile('document1.html', function(err,data) {
+//         res.writeHead(200, {'content-type': 'text/html' });
+//         res.write(data);
+//         res.end('Hello from server');
+//     });
+// }).listen(8080);
+
+const express = require('express');
+const app = express();
+var fs = require('fs');
+const port = 8080;
+
+app.get('/', (req, res) => {
+    fs.readFile('document1.html', function(err,data) {
+        res.writeHead(200, {'content-type': 'text/html' });
+        res.write(data);
+        res.end('Hello from server');
+    });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
